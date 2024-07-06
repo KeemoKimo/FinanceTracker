ba5a22d0bdeeecfd80bedaae0e99a11b2569403d
Keemo Kimo <thebunthong@gmail.com>
2024-07-06 11:10:00 -0300

    Finished setting up add data table

--- JS/mainPageJS.js
+++ JS/mainPageJS.js
@@ -2,3 +2,7 @@ function searchFunction(){
     var textbox = document.getElementById("txtSearch").value
     console.log(textbox)
 }
+
+function scrollToView(){
+    document.getElementById("addDataDiv").scrollIntoView()
+}
