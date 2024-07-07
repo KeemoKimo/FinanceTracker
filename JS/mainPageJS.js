4d98e4a19184494d71225f15a5325479a2568760
Keemo Kimo <thebunthong@gmail.com>
2024-07-07 08:35:12 -0300

    Adding value to Table
    successfully allowed the user to add new data entry to the table that can indicate their income, expenses, savings, and invested amount.
    
    Special Feature :
    Income > Expenses : the row of the table will be hightlighted in green
    
    Expenses > Income : the row of the table will be hightlighted in red

--- JS/mainPageJS.js
+++ JS/mainPageJS.js
@@ -8,6 +8,25 @@ function scrollToView() {
     document.getElementById("addDataDiv").scrollIntoView()
 }
 
+
+function insertTableData(dateStart, dateEnd, income, expenses, savings, invested){
+    var table = document.getElementById("tblData")
+    var row = table.insertRow(1)
+    var cellDate = row.insertCell(0)
+    var cellIncome = row.insertCell(1)
+    var cellExpenses = row.insertCell(2)
+    var cellBalance = row.insertCell(3)
+    cellDate.innerHTML = dateStart + " to " + dateEnd
+    cellIncome.innerHTML = "$ " + income
+    cellExpenses.innerHTML = "$ " + expenses
+    cellBalance.innerHTML = "<b>$ " + (Number(income) - Number(expenses)) + "</b>"
+    if((Number(income) - Number(expenses)) > 0){
+        row.style.background = "#ADF6B1"
+    }else if((Number(income) - Number(expenses)) < 0){
+        row.style.background = "#FF4B3E"
+    }
+}
+
 function addEntry() {
     var dateStart = document.getElementById("dateStart").value
     var dateEnd = document.getElementById("dateEnd").value
@@ -15,19 +34,15 @@ function addEntry() {
     var expenses = document.getElementById("txtExpenses").value
     var savings = document.getElementById("txtSavings").value
     var invested = document.getElementById("txtInvested").value
-    console.log(dateStart)
-    console.log(dateEnd)
-    console.log(income)
-    console.log(expenses)
-    console.log(savings)
-    console.log(invested)
     if (dateStart === "" || dateEnd === "" || income === "" || expenses === "" || savings === "" || invested === "") {
        alert("Please Enter in all the fields!")
     } else {
         if(dateStart > dateEnd){
             alert("Start date cannot be bigger than end date!")
         }else{
-            console.log("Date Check Pass")
+            document.getElementById("tr_Default").style.display = "none"
+            insertTableData(dateStart, dateEnd, income, expenses, savings, invested)
+            document.getElementById("addDataDiv").style.display = "none"
         }
     }
 }
