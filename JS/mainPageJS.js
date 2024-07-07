2d87bc423c39200f17fc11c59bc282f017bd8442
Keemo Kimo <thebunthong@gmail.com>
2024-07-07 08:13:53 -0300

    Added basic checks to "addEntry" section

--- JS/mainPageJS.js
+++ JS/mainPageJS.js
@@ -1,8 +1,33 @@
-function searchFunction(){
+function searchFunction() {
     var textbox = document.getElementById("txtSearch").value
     console.log(textbox)
 }
 
-function scrollToView(){
+function scrollToView() {
+    document.getElementById("addDataDiv").style.display = "block"
     document.getElementById("addDataDiv").scrollIntoView()
+}
+
+function addEntry() {
+    var dateStart = document.getElementById("dateStart").value
+    var dateEnd = document.getElementById("dateEnd").value
+    var income = document.getElementById("txtIncome").value
+    var expenses = document.getElementById("txtExpenses").value
+    var savings = document.getElementById("txtSavings").value
+    var invested = document.getElementById("txtInvested").value
+    console.log(dateStart)
+    console.log(dateEnd)
+    console.log(income)
+    console.log(expenses)
+    console.log(savings)
+    console.log(invested)
+    if (dateStart === "" || dateEnd === "" || income === "" || expenses === "" || savings === "" || invested === "") {
+       alert("Please Enter in all the fields!")
+    } else {
+        if(dateStart > dateEnd){
+            alert("Start date cannot be bigger than end date!")
+        }else{
+            console.log("Date Check Pass")
+        }
+    }
 }
